edd2e04a09c5e696e0f6ca8212ba97e7e9268420
Andrew Kennedy <akennedycode@gmail.com>
2020-07-16 23:05:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+happyHolidays() {
+  return "Happy Holidays!"
+}
+
+happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+happyCustomHolidayTo(holiday, name) {
+  return 'Happy ${holiday}, ${name}'
+}
+
+holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
